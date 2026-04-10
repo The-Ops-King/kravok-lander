19c3a98c12eb5583bddafb9fdea1f7c629f7d24f
Tyler Ray <tyler@kravok.ai>
2026-04-10 16:50:59 -0700

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? '/kravok-lander/' : '/',
   server: { port: 3333, strictPort: true, open: true },
 });
